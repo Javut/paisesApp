b2a1c928c05c7101930c1c93cfa918f1fb1fa456
Jaider Vergara <jvergarau@indracompany.com>
2023-03-10 11:31:32 -0500

    add @input event and @output

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./por-pais.component.css']
 })
 export class PorPaisComponent {
+
+  termino: string = "";
+  hayError: boolean = false;
+  paises : Country[] = [];
+
+  constructor(private paisService: PaisService ){}
+
+  buscar(termino: string){
+    this.hayError = false;
+    this.termino = termino;
+    console.log(this.termino);
+
+    this.paisService.buscarPais(this.termino).subscribe( (paises) => {
+      console.log(paises);
+      this.paises = paises;
+
+    }, (err) => {
+      this.hayError=true;
+      this.paises = [];
+    });
+
+
+  }
+
 
 }
